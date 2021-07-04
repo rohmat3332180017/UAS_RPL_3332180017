c4fc9f61d349ef1d1667a207759400c23fe3574c
rohmat3332180017 <rohmatstar2000@gmail.com>
2021-07-04 13:18:52 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="row" align="center">
+      <div className="column">
+        <div className="wrapper img1">
+          <div>Cathedral Valley</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img2">
+          <div>Nature beauty</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img3">
+          <div>Papaver Rhoeas</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img4">
+          <div>Great Sand Dunes</div>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="wrapper img5">
+          <div>Agriculture</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img6">
+          <div>Nature beauty</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img7">
+          <div>Out of This World</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img8">
+          <div>Camague, Frenhrich</div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="wrapper img9">
+          <div>Nature Collections</div>
+        </div>
+      </div>
+    <div className="footer">Gallery Created by Rohmat 3332180017 | Jurusan Teknik Elektro | Mata Kuliah Rekayasa Perangkat Lunak (RPL)</div>
+    </div>
+
+  );
+}
+
+export default App;
